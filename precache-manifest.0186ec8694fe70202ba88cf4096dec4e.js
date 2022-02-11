cdea4d50c8dd4b9be10b40b2a9795c172b3cd851
edray28 <edraytuyor@gmail.com>
2022-02-11 19:04:02 +0800

    Updates

---
+++ precache-manifest.0186ec8694fe70202ba88cf4096dec4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76be99158f415363e126c889ab0564d8",
+    "url": "/quote-generator-web/index.html"
+  },
+  {
+    "revision": "8b37a441a84f591cf7f9",
+    "url": "/quote-generator-web/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "c4ac0ad12011a2adc5dc",
+    "url": "/quote-generator-web/static/css/main.97393cbe.chunk.css"
+  },
+  {
+    "revision": "8b37a441a84f591cf7f9",
+    "url": "/quote-generator-web/static/js/2.1e98b21d.chunk.js"
+  },
+  {
+    "revision": "8c1eed750b87b1580399",
+    "url": "/quote-generator-web/static/js/3.2e8a5fb9.chunk.js"
+  },
+  {
+    "revision": "47c36d709edd3f63667e",
+    "url": "/quote-generator-web/static/js/4.11e747a6.chunk.js"
+  },
+  {
+    "revision": "c4ac0ad12011a2adc5dc",
+    "url": "/quote-generator-web/static/js/main.74d47053.chunk.js"
+  },
+  {
+    "revision": "534600547321b996cbdc",
+    "url": "/quote-generator-web/static/js/runtime~main.70215592.js"
+  }
+]);
